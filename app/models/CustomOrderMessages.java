d0842e94ffd5f2c3ee5722d0f21b3e09a20318fd
Annapurna Vemuri <annapurna.vmss@gmail.com>
2014-12-23 04:52:44 -0500

    Add messaging

--- app/models/CustomOrderMessages.java
+++ app/models/CustomOrderMessages.java
@@ -1,11 +1,13 @@
 package models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
 @Entity
 public class CustomOrderMessages extends Model {
 
@@ -30,4 +32,14 @@ public class CustomOrderMessages extends Model {
 	public String message;
 	public boolean fromCust;
 	public Date date_created;
+	
+	public static Finder<String,CustomOrderMessages> find = new Finder<String,CustomOrderMessages>(
+	        String.class, CustomOrderMessages.class
+	); 
+	
+	public static List<CustomOrderMessages> findByID(int id) {
+        return find.where()
+            .eq("order_id", id).orderBy("date_created")
+            .findList();
+    }
 }
--- app/models/Merchant.java
+++ app/models/Merchant.java
@@ -1,12 +1,16 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
 
 @Entity
 public class Merchant extends Model {
+	private static final long serialVersionUID = 1L;
 	@Id
 	public int merchant_id;
 	public String name;
@@ -15,4 +19,19 @@ public class Merchant extends Model {
 	public String password;
 	public boolean is_designer;
 	
+	public static Finder<String,Merchant> find = new Finder<String,Merchant>(
+	        String.class, Merchant.class
+	); 
+	
+	public static List<Merchant> findInvolving(String user) {
+        return find.where()
+            .eq("username", user)
+            .findList();
+    }
+	
+	public static List<Merchant> findUsingName(String user) {
+        return find.where()
+            .eq("name", user)
+            .findList();
+    }
 }
